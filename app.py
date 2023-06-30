4a61ac2266926d9d52626062df004d07de6ed03b
tom56561 <tom56561@gmail.com>
2023-06-30 22:57:45 +0800

    test POST method

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 import tornado.ioloop
 import tornado.web
+import pymongo
+import uuid
+import json
+from datetime import datetime, timedelta  # add this line
+
+
+
+# Setup MongoDB client
+client = pymongo.MongoClient('db', 27017)
+db = client.guid_db
+collection = db.guid_collection
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+    def post(self, guid=None):
+        # Extract metadata from request body
+        body = json.loads(self.request.body)
+        user = body.get('user')
+        expire = body.get('expire')
+        if not guid:
+            # Generate new GUID if not provided
+            guid = str(uuid.uuid4()).upper().replace('-', '')
+
+        # Create document
+        document = {
+            "_id": guid,
+            "user": user,
+            "expire": expire if expire else str(int((datetime.now() + timedelta(days=30)).timestamp()))
+        }
+        # Store in MongoDB and Redis
+        collection.insert_one(document)
+
+        self.write(document)
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/guid/?([a-fA-F0-9]{32})?", MainHandler),
     ])
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+import tornado.ioloop
+import tornado.testing
+import json
+from app import make_app 
+
+class TestApp(tornado.testing.AsyncHTTPTestCase):
+    def get_app(self):
+        return make_app()
+
+    def test_post(self):
+        post_data = {"user": "Cylance, Inc."}
+        response = self.fetch("/guid", method="POST", body=json.dumps(post_data))
+        # Expected HTTP status code is 200 for successful POST
+        self.assertEqual(response.code, 200)
+        response_body = json.loads(response.body)
+        # The response should contain a valid guid
+        self.assertEqual(len(response_body.get('guid')), 32)
+        self.assertEqual(response_body.get('user'), post_data.get('user'))
+
+if __name__ == "__main__":
+    unittest.main()
