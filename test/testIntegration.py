10e985c6621055879096c10e62eb296a62fbcdbc
tom56561 <tom56561@gmail.com>
2023-07-04 14:34:44 +0800

    add integration test

--- test/testIntegration.py
+++ test/testIntegration.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 import asyncio
+import time
 from tornado.testing import AsyncHTTPTestCase, gen_test
 from tornado.httpclient import AsyncHTTPClient
 
@@ -11,24 +12,101 @@ from src.cache import Cache
 class TestGUIDIntegration(AsyncHTTPTestCase):
     def get_app(self):
         return make_app(db=Database(), cache=Cache())
+    
+    @gen_test
+    async def test_async_behaviour_post(self):
+        guids = ["FA3A9A3A3A3A3A3A3A3A3A3A3A3A3A3" + str(i) for i in range(100)]
+
+        http_client = AsyncHTTPClient()
+
+        fetch_coroutines = [
+            http_client.fetch(
+                self.get_url(f"/guid/{guid}"),
+                method = "POST",
+                body = json.dumps({
+                    "guid": guid,
+                    "expire": int(time.time()) + 3600,
+                    "user": "Test user"
+                }),
+                headers = {"Content-Type": "application/json"},  # Specify that you're sending JSON
+                raise_error=False
+            ) 
+            for guid in guids
+        ]
+
+        responses = await asyncio.gather(*fetch_coroutines)
+
+        for response in responses:
+            self.assertEqual(response.code, 201)
 
     @gen_test
     async def test_async_behaviour_get(self):
-        # Prepare a bunch of urls to hit your API
-        guids = ["FA3A9A3A3A3A3A3A3A3A3A3A3A3A3A3A" for _ in range(1)]
+        guids = ["FA3A9A3A3A3A3A3A3A3A3A3A3A3A3A3" + str(i) for i in range(100)]
         urls = [self.get_url(f"/guid/{guid}") for guid in guids]
 
-        # Prepare the http_client
         http_client = AsyncHTTPClient()
 
-        # Prepare all the requests but do not start them
         fetch_coroutines = [http_client.fetch(url, method="GET", raise_error=False) for url in urls]
 
-        # Start all requests concurrently (not one after the other)
         responses = await asyncio.gather(*fetch_coroutines)
 
-        # Make assertions on the responses
         for response in responses:
-            self.assertIn(response.code, [200, 404])  # If the guid exists, it should return 200. If not, 404.
+            self.assertIn(response.code, [200, 404])
 
-    
+    @gen_test
+    async def test_async_behaviour_put(self):
+        guids = ["FA3A9A3A3A3A3A3A3A3A3A3A3A3A3A3" + str(i) for i in range(100)]
+        http_client = AsyncHTTPClient()
+
+        put_coroutines = [
+            http_client.fetch(
+                self.get_url(f"/guid/{guid}"),
+                method="PATCH",
+                body=json.dumps({
+                    "guid": guid,
+                    "expire": int(time.time()) + 7200,
+                    "user": f"Updated user {i}"
+                }),
+                headers={"Content-Type": "application/json"},
+                raise_error=False
+            )
+            for i, guid in enumerate(guids)
+        ]
+
+        responses = await asyncio.gather(*put_coroutines)
+
+        for response in responses:
+            self.assertEqual(response.code, 200)
+
+    @gen_test
+    async def test_async_behaviour_delete(self):
+        guids = ["FA3A9A3A3A3A3A3A3A3A3A3A3A3A3A3" + str(i) for i in range(100)]
+        http_client = AsyncHTTPClient()
+
+        delete_coroutines = [
+            http_client.fetch(
+                self.get_url(f"/guid/{guid}"),
+                method="DELETE",
+                raise_error=False
+            )
+            for guid in guids
+        ]
+
+        responses = await asyncio.gather(*delete_coroutines)
+
+        for response in responses:
+            self.assertEqual(response.code, 204)
+
+        get_coroutines = [
+            http_client.fetch(
+                self.get_url(f"/guid/{guid}"),
+                method="GET",
+                raise_error=False
+            )
+            for guid in guids
+        ]
+
+        responses = await asyncio.gather(*get_coroutines)
+
+        for response in responses:
+            self.assertEqual(response.code, 404)
