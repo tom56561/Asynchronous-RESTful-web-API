fbd800cec2bdf17b047b351a8852f30ee1e7150b
tom56561 <tom56561@gmail.com>
2023-07-04 19:34:10 +0800

    add integration test

--- test/testIntegration.py
+++ test/testIntegration.py
@@ -72,3 +72,96 @@ class TestGUIDIntegration(AsyncHTTPTestCase):
                 raise_error=False
             )
             self.assertEqual(response.code, 404)
+
+    # @gen_test
+    # async def test_async_behaviour_post(self):
+
+    #     http_client = AsyncHTTPClient()
+
+    #     fetch_coroutines = [
+    #         http_client.fetch(
+    #             self.get_url(f"/guid/{guid}"),
+    #             method = "POST",
+    #             body = json.dumps({
+    #                 "guid": guid,
+    #                 "expire": int(time.time()) + 3600,
+    #                 "user": "Test user"
+    #             }),
+    #             headers = {"Content-Type": "application/json"},  # Specify that you're sending JSON
+    #             raise_error=False
+    #         ) 
+    #         for guid in self.GUIDS
+    #     ]
+
+    #     responses = await asyncio.gather(*fetch_coroutines)
+
+    #     for response in responses:
+    #         self.assertEqual(response.code, 201)
+
+    # @gen_test
+    # async def test_async_behaviour_get(self):
+    #     urls = [self.get_url(f"/guid/{guid}") for guid in self.GUIDS]
+
+    #     http_client = AsyncHTTPClient()
+
+    #     fetch_coroutines = [http_client.fetch(url, method="GET", raise_error=False) for url in urls]
+
+    #     responses = await asyncio.gather(*fetch_coroutines)
+
+    #     for response in responses:
+    #         self.assertIn(response.code, [200, 404])
+
+    # @gen_test
+    # async def test_async_behaviour_put(self):
+    #     http_client = AsyncHTTPClient()
+
+    #     put_coroutines = [
+    #         http_client.fetch(
+    #             self.get_url(f"/guid/{guid}"),
+    #             method="PATCH",
+    #             body=json.dumps({
+    #                 "guid": guid,
+    #                 "expire": int(time.time()) + 7200,
+    #                 "user": f"Updated user {i}"
+    #             }),
+    #             headers={"Content-Type": "application/json"},
+    #             raise_error=False
+    #         )
+    #         for i, guid in enumerate(self.GUIDS)
+    #     ]
+
+    #     responses = await asyncio.gather(*put_coroutines)
+
+    #     for response in responses:
+    #         self.assertEqual(response.code, 200)
+
+    # @gen_test
+    # async def test_async_behaviour_delete(self):
+    #     http_client = AsyncHTTPClient()
+
+    #     delete_coroutines = [
+    #         http_client.fetch(
+    #             self.get_url(f"/guid/{guid}"),
+    #             method="DELETE",
+    #         )
+    #         for guid in self.GUIDS
+    #     ]
+
+    #     responses = await asyncio.gather(*delete_coroutines)
+
+    #     for response in responses:
+    #         self.assertEqual(response.code, 204)
+
+        # get_coroutines = [
+        #     http_client.fetch(
+        #         self.get_url(f"/guid/{guid}"),
+        #         method="GET",
+        #         raise_error=False
+        #     )
+        #     for guid in self.GUIDS
+        # ]
+
+        # responses = await asyncio.gather(*get_coroutines)
+
+        # for response in responses:
+        #     self.assertEqual(response.code, 404)
